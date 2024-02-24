bfd1f7a7362c0c8f44251ad56daa2dac6aa48cc0
RachelDfs <94901471+RachelDfs@users.noreply.github.com>
2024-02-24 23:44:54 +0100

    modifs camera home + graphic update intro laverie
    correction mipmaps / meshes UVs machine a laver / lighting + add peinture mongolfiere

--- scripts/home_laverrie_logic.gd
+++ scripts/home_laverrie_logic.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	camera.rotation.z = -0.04
+	camera.rotation.z = -0.01
 	var tween : Tween = create_tween().set_parallel(false).set_loops().set_trans(Tween.TRANS_SINE)
-	tween.tween_property(camera, "rotation:z", 0.04, 2)
-	tween.tween_property(camera, "rotation:z", -0.04, 2)
+	tween.tween_property(camera, "rotation:z", 0.01, 2)
+	tween.tween_property(camera, "rotation:z", -0.01, 2)
