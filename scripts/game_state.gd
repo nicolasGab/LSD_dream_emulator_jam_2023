dc3799a1412b1b7d6c31187855df959e05aaba53
Nicolas Gaborel <nicolas.gaborel@gmail.com>
2024-02-25 20:09:25 +0100

    Remove final scene loop, always loop on scenes

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -4,7 +4,6 @@ extends Node
 @export var gravity : float = 0.6
 @export var eventbus: Node
 @export var dreams: Array[PackedScene]
-@export var final_scene: PackedScene
 
 var object_enabled : bool = false
 
@@ -13,8 +12,4 @@ func _ready():
 
 func next_scene():
 	if dreams.size() > 0:
-		var scene = dreams.pick_random()
-		get_tree().change_scene_to_packed(scene)
-		dreams.erase(scene)
-	else :
-		get_tree().change_scene_to_packed(final_scene)
+		get_tree().change_scene_to_packed(dreams.pick_random())
